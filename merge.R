7d7712b82ee5ca72715484b8883285993b41c545
Alexander Rezvykh <aprezvykh@yandex.ru>
2017-09-14 13:41:46 +0300

    function add

--- merge.R
+++ merge.R
@@ -1,3 +1,6 @@
+### Script finds top genes from two data sets, that are overexpressed, but heve minimum differential expressed
+### avg is BaseMean/gene counts. It can be made with DESeq2 from DEFlow script in this git
+
 moto.avg <- 609.685
 exp.avg <- 637.596
   
@@ -6,6 +9,8 @@ motoneurons <- as.data.frame(read.csv("~/motoneurons compare/motoneurons.csv"))
 
 common <- intersect(experimental$X, motoneurons$X)
 common <- as.list(common)
+
+dfdiff <- function(mer_exp, mer_moto) {
 mer_exp <- data.frame()
 mer_moto <- data.frame()
 for (i in common){
@@ -20,7 +25,7 @@ for (i in common){
   mer_moto <- rbind(motoneurons[m,], mer_moto)
 }
 
-mer_bm <- data.frame(mer_exp$X, mer_exp$symbol, mer_exp$name, mer_exp$baseMean, mer_moto$baseMean)
+mer_bm <- data.frame(mer_exp$X, mer_exp$symbol, mer_exp$name, mer_exp$entrez, mer_exp$baseMean, mer_moto$baseMean)
 
 mer_bm$mer_exp.baseMean <- as.numeric(as.character(mer_bm$mer_exp.baseMean))/exp.avg
 mer_bm$mer_moto.baseMean <- as.numeric(as.character(mer_bm$mer_moto.baseMean))/moto.avg
@@ -29,3 +34,7 @@ mer_bm$Delta <- mer_bm$mer_exp.baseMean/mer_bm$mer_moto.baseMean
 sub <- as.data.frame(subset(mer_bm, Delta > 0.9))
 sub_low <- as.data.frame(subset(sub, Delta < 1.1))
 
+return(sub_low)
+}
+
+dataset <- dfdiff(mer_exp, mer_moto) 
