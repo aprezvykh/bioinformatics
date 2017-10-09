6d33f98c08aa103d192f21709ebddc277813ae09
aprezvykh <aprezvykh@yandex.ru>
2017-10-09 21:13:34 +0300

    DEG

--- supplementary scripts/DEFlow enrichment from CSV.R
+++ supplementary scripts/DEFlow enrichment from CSV.R
@@ -1,46 +1,130 @@
 ## GO & REACTOME ENRICHMENT FROM CSV FILE.
 ## NOT WORKING WITH DROSOPHILA
-
 library("gage")
 library("gageData")
-df <- read.table(file = "~/counts_ens/manifestation.csv", sep = ",", header= TRUE)
-df <- df[,colSums(is.na(df))<nrow(df)]
-foldchanges = df$log2FoldChange
-names(foldchanges) = df$entrez
-### GO ###
+library("pathview")
+library("dplyr")
+library("ReactomePA")
+library("reactome.db")
+library("topGO")
+library("clusterProfiler")
+gs_size <- 1
+kegg_plots <- TRUE
 data(go.sets.mm)
 data(go.subs.mm)
+setwd("~/bioinformatics/counts/ALS Mice/compare_new/glia/")
+et_annot <- read.csv("~/bioinformatics/counts/ALS Mice/compare_new/deg_glia.csv")
+rownames(et_annot) <- et_annot$X
+et_annot_high <- subset(et_annot, et_annot$logFC > 0)
+et_annot_low <- subset(et_annot, et_annot$logFC < 0)
+foldchanges = et_annot$logFC
+names(foldchanges) = et_annot$entrez
+
 gomfsets = go.sets.mm[go.subs.mm$MF]
-gomfres = gage(foldchanges, gsets=gomfsets, same.dir=TRUE)
+gomfres = gage(foldchanges, gsets=gomfsets, same.dir=TRUE,set.size = c(10, 100), rank.test = TRUE)
 lapply(gomfres, head)
-write.xlsx(gomfres, file = "GOMF.xlsx", sheetName = "Common info")
+gomfres <- as.data.frame(gomfres)
+gomfres <- gomfres[complete.cases(gomfres), ]
+write.xlsx(gomfres, file = "GO.xlsx", sheetName = "GO_MF", append = TRUE)
 
 gobpsets = go.sets.mm[go.subs.mm$BP]
 gobpres = gage(foldchanges, gsets=gobpsets, same.dir=TRUE)
 lapply(gobpres, head)
-write.xlsx(gobpres, file = "GOBP.xlsx", sheetName = "Common info")
-
+gobpres <- as.data.frame(gobpres)
+gobpres <- gobpres[complete.cases(gobpres), ]
+write.xlsx(gobpres, file = "GO.xlsx", sheetName = "GO_BP", append = TRUE)
 
 goccsets = go.sets.mm[go.subs.mm$CC]
 goccres = gage(foldchanges, gsets=goccsets, same.dir=TRUE)
 lapply(goccres, head)
-write.xlsx(gobpres, file = "GOCC.xlsx", sheetName = "Common info")
+goccres <- as.data.frame(goccres)
+goccres <- goccres[complete.cases(goccres), ]
+write.xlsx(goccres, file = "GO.xlsx", sheetName = "GO_CC", append = TRUE)
 
 
-### (foldchanges)
-require(clusterProfiler)
-require(reactome.db)
-
-df <- as.character(df$entrez)
-print(df)
-summary(df)
-class(df)
-x = enrichPathway(gene=df, organism = "mouse", minGSSize=10, readable = TRUE )
-write.csv(x, file = "GSEA.csv")
+dfa <- as.character(et_annot$entrez)
+x <- enrichPathway(gene=dfa, organism = "mouse", minGSSize=gs_size, readable = TRUE )
 head(as.data.frame(x))
-dotplot(x, showCategory=12)
-enrichMap(x, layout=igraph::layout.kamada.kawai, vertex.label.cex = 0.7 )
-cnetplot(x, categorySize="pvalue", foldChange=foldchanges)
+dev.off()
 
-cnetplot()
+par(mar=c(1,1,1,1))
+pdf(file = "barplot.pdf", width = 12, height = 17, family = "Helvetica")
+barplot(x, showCategory=30,  font.size = 9)
+dev.off()
 
+pdf(file = "enrichmap.pdf", width = 12, height = 17, family = "Helvetica")
+enrichMap(x, layout=igraph::layout.kamada.kawai, vertex.label.cex = 0.7, n = 20, font.size = 20)
+dev.off()
+
+pdf(file = "cnetplot.pdf", width = 12, height = 17, family = "Helvetica")
+cnetplot(x, foldChange = foldchanges, categorySize="pvalue", showCategory = 10)
+dev.off()
+
+#HIGH
+df_high <- et_annot_high$entrez
+x <- enrichPathway(gene=df_high, organism = "mouse", minGSSize=gs_size, readable = TRUE )
+head(as.data.frame(x))
+dev.off()
+
+par(mar=c(1,1,1,1))
+pdf(file = "barplot_high.pdf", width = 12, height = 17, family = "Helvetica")
+barplot(x, showCategory=30,  font.size = 9)
+dev.off()
+
+#LOW
+
+df_low <- et_annot_low$entrez
+x <- enrichPathway(gene=df_low, organism = "mouse", minGSSize=gs_size, readable = TRUE )
+head(as.data.frame(x))
+dev.off()
+
+par(mar=c(1,1,1,1))
+pdf(file = "barplot_low.pdf", width = 12, height = 17, family = "Helvetica")
+barplot(x, showCategory=30,  font.size = 9)
+dev.off()
+
+
+
+###KEGG expression profile (without lfc, but with generatio)
+
+kk <- enrichKEGG(gene = df_high, organism = "mmu", pvalueCutoff = 0.05)
+write.xlsx(kk, file = "KEGG.xlsx", sheetName = "KEGG_upreg", append = TRUE)
+pdf(file = "KEGG_upreg.pdf", width = 12, height = 17, family = "Helvetica")
+barplot(kk, showCategory=3,  font.size = 9)
+dev.off()
+
+kk <- enrichKEGG(gene = df_low, organism = "mmu", pvalueCutoff = 0.05)
+write.xlsx(kk, file = "KEGG.xlsx", sheetName = "KEGG_downreg", append = TRUE)
+pdf(file = "KEGG_downreg.pdf", width = 12, height = 17, family = "Helvetica")
+barplot(kk, showCategory=3,  font.size = 9)
+dev.off()
+
+
+data(kegg.sets.mm)
+data(sigmet.idx.mm)
+kegg.sets.mm = kegg.sets.mm[sigmet.idx.mm]
+
+keggres = gage(foldchanges, gsets=kegg.sets.mm, same.dir=TRUE)
+keggreswr <- as.data.frame(keggres)
+write.xlsx(keggreswr, file = "KEGG pathview.xlsx", sheetName = "KEGG", append = TRUE)
+
+if (kegg_plots == TRUE){
+  keggrespathways = data.frame(id=rownames(keggres$greater),
+                               keggres$greater) %>% 
+    tbl_df() %>% 
+    filter(row_number()<=20) %>% 
+    .$id %>% 
+    as.character()
+  keggresids = substr(keggrespathways, start=1, stop=8)
+  
+  plot_pathway = function(pid){
+    pathview(gene.data=foldchanges, 
+             pathway.id=pid, 
+             species="mmu", 
+             new.signature=FALSE)
+  }
+  detach("package:dplyr", unload=TRUE)
+  dir.create("kegg")
+  setwd("kegg")
+  tmp = sapply(keggresids, function(pid) pathview(gene.data=foldchanges, pathway.id=pid, species="mmu"))
+}
