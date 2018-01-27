6159dbfb968c57bcd9ee54347352ec9398400975
aprezvykh <aprezvykh@yandex.ru>
2018-01-27 20:37:28 +0300

    coverage add

---
+++ coverage.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+dir <- c('~/coverage/')
+setwd(dir)
+files <- grep("txt", list.files(dir), value = TRUE)
+df <- data.frame()
+a <- data.frame()
+for (f in files){
+  a <- read.table(file = paste(f), header = TRUE)
+  a$Percentile <- NULL
+  a <- as.data.frame(a)
+  rownames(a) <- paste(f)
+  df <- rbind(a, df)
+}
+
+leg <- rownames(df)
+df <- as.data.frame(t(df))
+
+
+pdf("Coverage.pdf", height = 10, width = 10, family = "Helvetica")
+matplot(df,type = "l", pch=1, col = 1:12, xlab="Gene body percentile (5'->3')", ylab="Coverage",lwd=0.8) #plot
+legend("topleft", legend = leg, col=1:12, pch=1)
+dev.off()
