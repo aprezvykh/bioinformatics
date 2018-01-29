bb9da52c4cecbed3b91ae5fc92fa7585f582e21a
aprezvykh <aprezvykh@yandex.ru>
2018-01-29 21:15:20 +0300

    bias

--- bias.R
+++ bias.R
@@ -1,37 +1,13 @@
 source("https://bioconductor.org/biocLite.R")
-biocLite("Rsamtools")
 biocLite("seqbias")
 library("seqbias")
-library("Rsamtools")
+library(Rsamtools)
+library(devtools)
 
-readBAM <- function(bamFile){
-  
-  bam <- scanBam(bamFile)
-  
 
-  .unlist <- function (x){
-    x1 <- x[[1L]]
-    if (is.factor(x1)){
-      structure(unlist(x), class = "factor", levels = levels(x1))
-    } else {
-      do.call(c, x)
-    }
-  }
-  
-  bam_field <- names(bam[[1]])
-  
-  list <- lapply(bam_field, function(y) .unlist(lapply(bam, "[[", y)))
-  
-  bam_df <- do.call("DataFrame", list)
-  names(bam_df) <- bam_field
-
-  return(bam_df)
-}
-
-reads_fn <- readBAM("~/BAM/1.bam")
-
-ref_fn <- read.fasta(file = system.file("~/BAM/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa", package = "seqbias")
+ref_fn <- "~/BAM/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa"
 ref_f <- FaFile(ref_fn)
-
 open.FaFile(ref_f)
-reads_fn <- system.file("~/BAM/1.bam", package = "seqbias")
+ref_seqs <- scanFaIndex(ref_f)
+I <- random.intervals(ref_seqs, n = 5, m = 100000)
+seqs <- scanFa(ref_f, I)
