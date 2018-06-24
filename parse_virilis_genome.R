744acab49ef6dc71353be9f85ab8f02fddf51a9b
Alexander <aprezvykh@yandex.ru>
2018-06-25 00:05:50 +0300

    sas

--- parse_virilis_genome.R
+++ parse_virilis_genome.R
@@ -1,18 +1,27 @@
 library(GenomicRanges)
 rg <- rtracklayer::import('~/Documents/194.226.21.15/Rezvykh.stuff/RepMask/dvir-all-r1.06.gtf')
-df <- rg[which(rg$type == "mRNA"),]
-df <- as.data.frame(df)
-
-
+#df <- rg[which(rg$type == "mRNA"),]
+df <- as.data.frame(rg)
 
 for (f in unique(df$seqnames)){
   sub <- df[which(df$seqnames == f),]
-  ss <- sub[which(sub$type == "mRNA"),]
+  ss <- sub[which(sub$type == "start_codon"),]
     for (i in 1:nrow(ss)){
       a <- as.numeric(ss$start[i+1]) - as.numeric(ss$start[i])
       i <- i+1
-      if(abs(a) > 200000){
-        print(paste(f, "-", df$start, ":",df$end, sep = ""))
+      if(is.na(a) == TRUE){
+        next
+      } else if (is.null(a) == TRUE){
+        next
+      } else if (length(a) < 1){
+        next
+      }
+      if(abs(as.numeric(a)) > 200000 | abs(as.numeric(a) < 300000)){
+      print(paste(f, "-", ss$start[i+1], ":",ss$end[i], sep = ""))
+      } else {
+        next
       }
     }
 }
+
+
