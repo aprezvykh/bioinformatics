b0b1b292d24b6e418178fad6b95e3b18bd07f130
aprezvykh <aprezvykh@yandex.ru>
2019-05-10 14:34:07 +0300

    patent

--- patent.R
+++ patent.R
@@ -1,9 +1,16 @@
 library(tm)
 library(ggplot2)
+library(stringr)
 setwd("~/")
 stop.words <- stopwords(kind = "en")
+countries.letter.codes <- read.csv("patent/all.csv", stringsAsFactors = F)
+
 '%!in%' <- function(x,y)!('%in%'(x,y))
+substrRight <- function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
 df <- read.csv("patent.csv", header = T,stringsAsFactors = F)
+
 
 
 # Выявление наиболее часто встречающихся в выборке патентодателей ---------
@@ -60,3 +67,33 @@ wordcloud::wordcloud(words = wf$Var1, freq = wf$Freq,
                      colors=brewer.pal(8, "Dark2"))
 dev.off()
 
+
+
+# Распределение по странам ------------------------------------------------
+
+inventor <- df$Изобретатель.и.
+inventor <- unlist(strsplit(inventor, "\n"))
+inventor.wc <- unique(inventor[grep("\\[*\\]", inventor)])
+countries <- substrRight(inventor.wc, 4)
+countries <- gsub("[", "", countries, fixed = T)
+countries <- gsub("]", "", countries, fixed = T)
+
+cnt.frq <- data.frame(table(t(countries)))
+
+cnt.frq$country <- countries.letter.codes[match(cnt.frq$Var1, countries.letter.codes$alpha.2),]$name
+
+png("patent/4_countries.png", height = 250, width = 250, res = 600, units = "mm")
+ggplot(data=cnt.frq) + geom_bar(aes(x = reorder(country, -Freq), y = Freq), stat = "identity") + 
+  theme(text=element_text(family="Liberation Serif", face="bold", size=12,colour = "black")) + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
+        panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black",size = 0.3),
+        panel.border = element_rect(colour = "black", fill=NA, size=0.3),
+        axis.text=element_text(size=12, color = "black", face = "bold"),
+        axis.title=element_text(size=12,face="bold", color = "black"),
+        axis.line.x = element_line(color = "black", size = 0.3),
+        axis.line.y = element_line(color = "black",size = 0.3)) + 
+  xlab("Страна") + 
+  ylab("Количество патентов")
+dev.off()
+                                
